29a9d02a8b89344f4d3272ffbfd64b2349cf95d9
mac <hendriarga28@gmail.com>
2024-12-06 21:44:40 +0700

    Register & Category API

---
+++ app/Http/Controllers/API/Register/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API\Register;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\RegisterResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request): JsonResponse|RegisterResource
+    {
+        $request->validated();
+
+        $data = [
+          ...$request->except('password'),
+            'password' => bcrypt($request->password),
+            'role_id' => 3,
+            'is_active' => 0
+        ];
+
+        $user = User::query()->create($data);
+        $sanctumToken = config('app.sanctum_token');
+        $token = $user->createToken($sanctumToken, ['*'], now()->addMonths(6))->plainTextToken;
+
+        return (new RegisterResource($user))->additional([
+            'success' => (bool) $user,
+            'message' => 'User registered successfully',
+            'token' => $token,
+        ]);
+    }
+}
